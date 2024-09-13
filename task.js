ce0516d79aa7dbf3e60c9c7ed5d67efbd0d52dcc
toseef39 <toseefkalwar2001@gmail.com>
2024-09-13 19:29:14 +0500

    task

---
+++ task.js
@@ -0,0 +1,27 @@
+let  a = prompt( "What kind of this number?")
+//  a =1 
+
+if ( a % 2 == 0 ) {
+    // let a = "Atique"
+    console.log(a)
+    
+    alert("This is even  number")
+}
+else {
+    alert("This is odd number")
+}
+// console.log(a)
+// let a = prompt("What types of this number?")
+  
+//   if (a%2 == 0) {
+//      alert ("This is even number ")
+//   }
+//   else if (a % a == 0 && a % 1 == 0 ) {
+//       alert ( " This is prime number ")
+//   }
+//   else if (a % 2 !==0){
+//     alert("This is odd number ")
+//   }
+//   else{
+//     alert("not valid ")
+//   }
